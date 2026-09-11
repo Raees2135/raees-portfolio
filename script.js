96be2d593ab13607bc569143c8a4c1af37afaa71
mohd-raees <mohdraees@carinasoftlabs.in>
2026-09-12 00:06:03 +0530

    added resume

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-const menu=document.querySelector('.menu');const nav=document.querySelector('.nav');menu?.addEventListener('click',()=>nav.classList.toggle('open'));document.querySelectorAll('.nav nav a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')));
+const menu = document.querySelector('.menu');
+const nav = document.querySelector('.nav');
+
+menu?.addEventListener('click', () => {
+  nav?.classList.toggle('open');
+});
+
+document.querySelectorAll('.nav nav a').forEach((a) => {
+  a.addEventListener('click', () => {
+    nav?.classList.remove('open');
+  });
+});
